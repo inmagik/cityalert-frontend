111b2e89bd3390c49aba4650cd7d0bde261ab02c
gffuma <fumagalli.gf@gmail.com>
2018-09-08 23:06:45 +0200

    Filters

--- src/pages/Alerts.js
+++ src/pages/Alerts.js
@@ -6,27 +6,48 @@ import SubNavbar from '../components/SubNavbar'
 import BottomTabs from '../components/BottomTabs'
 import { connect } from 'react-redux'
 import { loadAlerts, getAlerts } from '../state/alerts'
+import { loadAlertTypes, getAlertTypes } from '../state/alertTypes'
+import debounce from 'lodash/debounce'
+import qs from 'query-string'
 
 class Alerts extends PureComponent {
-
-
   componentDidMount() {
     this.props.loadAlerts()
+    this.props.loadAlertTypes()
   }
 
   toggleTab = (tabName) => () => {
     this.props.history.push(`/alerts/${tabName}`)
   }
 
+  onChangeFilter = values => {
+    this.props.history.push(`/alerts?${qs.stringify(values)}`)
+    this.props.loadAlerts(values)
+  }
+
+  onChangeFilterDebounced = values => {
+    this.props.history.push(`/alerts?${qs.stringify(values)}`)
+    this.loadDebounced(values)
+  }
+
+  loadDebounced = debounce((params) => {
+    this.props.loadAlerts(params)
+  }, 300)
+
   render() {
-    const { alerts, match } = this.props
+    const { alerts, alertTypes, match, location } = this.props
     const { params } = match
     const activeTab = params.activeTab || 'list'
+    const filters = qs.parse(location.search)
 
-    console.log(alerts)
     return (
       <Layout>
-        <SubNavbar />
+        <SubNavbar
+          values={filters}
+          onChangeFilter={this.onChangeFilter}
+          onChangeFilterDebounced={this.onChangeFilterDebounced}
+          alertTypes={alertTypes}
+        />
         <div className='alerts-container'>
           {activeTab === 'list' && <AlertsList alerts={alerts}/>}
           {activeTab === 'map' && <AlertsMap alerts={alerts}/>}
@@ -40,6 +61,8 @@ class Alerts extends PureComponent {
 const emptyList = []
 export default connect(state => ({
   alerts: getAlerts(state) || emptyList,
+  alertTypes: getAlertTypes(state) || emptyList,
 }), {
   loadAlerts,
+  loadAlertTypes,
 })(Alerts)
