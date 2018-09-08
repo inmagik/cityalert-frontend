d108e7112f003dd50722fcb083fdffab7048c037
gffuma <fumagalli.gf@gmail.com>
2018-09-08 16:46:27 +0200

    Add basic stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/css/all.css'
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
---
+++ src/pages/Logi.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => (
+  <div>
+    23
+  </div>
+)
+
+export default Login
