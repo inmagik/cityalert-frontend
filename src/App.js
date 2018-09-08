3e53d3ffbd41a87fa06772fd6ae5ae73595bad97
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2018-09-08 16:59:04 +0200

    fixing app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AlertList from './pages/AlertList'
-
-class App extends Component {
-  render() {
-    return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
-      <AlertList />
-    );
-  }
-}
-
-export default App;
-=======
 import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
@@ -43,4 +18,3 @@ const App = () => (
   </Provider>
 )
 export default App
->>>>>>> d108e7112f003dd50722fcb083fdffab7048c037
