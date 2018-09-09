b806a807fa104abc1465de6b9eb10da600416728
Mauro Bianchi <bianchimro@gmail.com>
2018-09-09 12:21:29 +0200

    top nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,22 +11,23 @@ class Navbar extends PureComponent {
       <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div className="navbar-nav mr-auto">
           <Link className="navbar-brand" to='/alerts/list'>CityAlert</Link>
-          <div className="nav-link">
-            <Link className="navbar-link" to='/user-alerts'>{user.email}</Link>
-          </div>
 
           {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button> */}
         </div>
+        <span className="nav-link">
+          <Link className="navbar-link" to='/user-alerts'>{user.email}</Link>
+        </span>
+
         <Route path='/alerts/(list|map)' exact component={() => (
           <Link className="btn btn-outline-primary" to='/alerts/add'>
-            <i className="fa fa-plus"></i>{' '}Aggiungi segnalazione
+            <i className="fa fa-plus"></i>
           </Link>
         )} />
         <Route path='/alerts' exact component={() => (
           <Link className="btn btn-outline-primary" to='/alerts/add'>
-            <i className="fa fa-plus"></i>{' '}Aggiungi segnalazione
+            <i className="fa fa-plus"></i>
           </Link>
         )} />
 
